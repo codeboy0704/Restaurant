77d77ff1c1b7ad4807a38c1b6ed17d52a89af286
codeboy0407 <ianm2927@gmail.com>
2023-03-23 09:06:33 -0500

    carrusel v1-pending(modify text of each slide, style for arrows)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import Presentation from "./components/main/Presentation";
 import Menu from "./components/menu/Menu";
+import Carrusel from "../src/components/carrusel/Swiper";
+import elements from "./components/carrusel/elements";
 
 function App() {
   return (
     <div className="App">
       <Menu />
       <Presentation />
+      <Carrusel elements={elements} />
     </div>
   );
 }
---
+++ src/components/carrusel/elements.js
@@ -0,0 +1,8 @@
+import Breakfast from "../../assets/food/breakfast.jpg";
+import Dessert from "../../assets/food/dessert.jpg";
+import Meat from "../../assets/food/meat-ga03697fd2_1280.jpg";
+import Meat2 from "../../assets/food/food-gf7b22b116_1280.jpg";
+
+const elements = [Breakfast, Dessert, Meat, Meat2];
+
+export default elements;
