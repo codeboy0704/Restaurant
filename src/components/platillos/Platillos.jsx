8d4a3c31a3018ea77a9bcf143d21aefa22750b1d
codeboy0407 <ianm2927@gmail.com>
2023-03-25 16:30:23 -0500

    platillos section description added(mobile design)

--- src/components/platillos/Platillos.jsx
+++ src/components/platillos/Platillos.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import platillos from "./platillosData";
+import Logo from "../../assets/our_food.svg";
 function Platillos() {
   const platillosList = platillos.map((el) => {
     return (
@@ -12,7 +13,36 @@ function Platillos() {
       </div>
     );
   });
-  return <div className="platillos_section">{platillosList}</div>;
+  return (
+    <div className="platillos_section">
+      {platillosList}
+      <div className="platillos_description">
+        <h1>Todos aman nuestra comida.</h1>
+        <div className="platillos_description_logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="platillos_description_text">
+          <p>
+            Un arco de veraneras recibe a los visitantes junto a nuestro
+            bucanero de alta mar para tomarse fotos antes o después de disfrutar
+            de una velada, o simplemente al pasar por el lugar. Los turistas
+            locales e internacionales se maravillan con nuestra carta,
+            amenidades, precios y servicio al cliente. Las reseñas hablan por sí
+            solas.
+          </p>
+          <p>
+            Para los tour operadores, contamos con servicios y paquetes
+            especiales, en aras a impulsar el turismo nacional y comercio local.
+            Nuestro salón de banquetes es ideal para reuniones de trabajo o
+            celebraciones y planes de lealtad corporativos con descuentos y
+            promociones especiales.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+// rgb(97 73 153 / 90%)
 
 export default Platillos;
