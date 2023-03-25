8d441a9fab1da284b6d69e439f67d403f085e1af
codeboy0407 <ianm2927@gmail.com>
2023-03-25 15:55:34 -0500

    platillos section(mobile)-done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Presentation from "./components/main/Presentation";
 import Menu from "./components/menu/Menu";
 import Carrusel from "../src/components/carrusel/Swiper";
 import elements from "./components/carrusel/elements";
+import Platillos from "./components/platillos/Platillos";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Menu />
       <Presentation />
       <Carrusel elements={elements} />
+      <Platillos />
     </div>
   );
 }
