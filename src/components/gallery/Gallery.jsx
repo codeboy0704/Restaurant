e7b1b5b9d27397bef85033f16d1f8e1dee779da0
Ian Flores <ianflores07@pop-os.localdomain>
2023-04-21 21:42:52 -0500

    gallery component added

---
+++ src/components/gallery/Gallery.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Gallery() {
+  return (
+    <div className='gallery_container'>
+        <div className='gallery_main'>
+ 
+        </div>
+    </div>
+  )
+}
+
+export default Gallery
